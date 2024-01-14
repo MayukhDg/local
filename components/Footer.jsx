d68cde4edb0aa85348e5c3293c872f6ad96870d2
Mayukh <mayukh1995@gmail.com>
2024-01-14 19:57:33 +0530

    footer fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,11 +3,6 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='h-[80vh] flex flex-col'>
-     <div className='flex flex-1 items-center' >
-     <img
-      src="/gl.png"
-     />
-     </div>
 
     </footer>
   )
